cb1d0372eb39fafad99f0a71e93e2f5bee7233ba
Gabriel Nascimento <128855690+GabNasci@users.noreply.github.com>
2024-05-06 19:50:18 -0300

    Adicionando arquivos TDE 6

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const port = 5000
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: "running"
+    })
+})
+
+
+app.listen(port, () =>{
+    console.log(`Rodando na porta: ${port}`)
+})
